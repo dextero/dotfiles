1b8690d777f203f309eb6ff8e7054af96023b7a5
Marcin Radomski <marcin@mradomski.pl>
2023-08-05 13:09:35 +0200

    Add neovim init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,192 @@
+-- ensure the packer plugin manager is installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require("packer").startup(function(use)
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
+  -- Collection of common configurations for the Nvim LSP client
+  use("neovim/nvim-lspconfig")
+  -- Visualize lsp progress
+  use({
+    "j-hui/fidget.nvim",
+    tag = "legacy",
+    config = function()
+      require("fidget").setup()
+    end
+  })
+
+  -- Autocompletion framework
+  use("hrsh7th/nvim-cmp")
+  use({
+    -- cmp LSP completion
+    "hrsh7th/cmp-nvim-lsp",
+    -- cmp Snippet completion
+    "hrsh7th/cmp-vsnip",
+    -- cmp Path completion
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+    after = { "hrsh7th/nvim-cmp" },
+    requires = { "hrsh7th/nvim-cmp" },
+  })
+  -- See hrsh7th other plugins for more great completion sources!
+  -- Snippet engine
+  use('hrsh7th/vim-vsnip')
+  -- Adds extra functionality over rust analyzer
+  use("simrat39/rust-tools.nvim")
+
+  -- Optional
+  use("nvim-lua/popup.nvim")
+  use("nvim-lua/plenary.nvim")
+  use("nvim-telescope/telescope.nvim")
+
+  -- Some color scheme other then default
+  use("nanotech/jellybeans.vim")
+  use("arcticicestudio/nord-vim")
+
+  -- Seamless navigation between vim splits and tmux panes
+  use("christoomey/vim-tmux-navigator")
+
+  use("kien/ctrlp.vim")
+
+  -- Fancy statusline
+  use("famiu/feline.nvim")
+  use("rstacruz/sparkup")
+end)
+
+-- the first run will install packer and our plugins
+if packer_bootstrap then
+  require("packer").sync()
+  return
+end
+
+-- Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not auto-select, nvim-cmp plugin will handle this for us.
+vim.o.completeopt = "menuone,noinsert,noselect"
+
+-- Avoid showing extra messages when using completion
+vim.opt.shortmess = vim.opt.shortmess + "c"
+
+local function on_attach(client, buffer)
+  -- This callback is called when the LSP is atttached/enabled for this buffer
+  -- we could set keymaps related to LSP, etc here.
+end
+
+-- Configure LSP through rust-tools.nvim plugin.
+-- rust-tools will configure and enable certain LSP features for us.
+-- See https://github.com/simrat39/rust-tools.nvim#configuration
+local opts = {
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+}
+
+require("rust-tools").setup(opts)
+
+-- Setup Completion
+-- See https://github.com/hrsh7th/nvim-cmp#basic-configuration
+local cmp = require("cmp")
+cmp.setup({
+  preselect = cmp.PreselectMode.None,
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    -- Add tab support
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+  },
+
+  -- Installed sources
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+    { name = "path" },
+    { name = "buffer" },
+  },
+})
+
+-- Colorscheme tweaks
+vim.cmd([[
+set termguicolors
+colorscheme jellybeans
+
+" Dim comments
+hi Normal ctermbg=None gui=None
+hi NonText ctermbg=None gui=None
+hi Comment cterm=None gui=None ctermfg=238 guifg=#444444
+
+" Diff color tweaks
+hi DiffAdd ctermbg=22 guifg=#005f00
+hi DiffChange ctermbg=100 guifg=#878700
+hi DiffDelete ctermbg=52 guifg=#5f0000
+
+" Highlight tabs and EOL whitespaces
+highlight ExtraWhitespace ctermbg=red guibg=red
+match ExtraWhitespace /\s\+$/
+autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
+autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+autocmd InsertLeave * match ExtraWhitespace /\s\+$/
+autocmd BufWinLeave * call clearmatches()
+]])
+
+require("feline").setup()
+
+-- Tab handling stuff
+vim.cmd([[
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set expandtab
+]])
